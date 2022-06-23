a780391359530247c1b6b49bda94107e8ba5a967
chathura.dev93 <chathura.dev93@gmail.com>
2022-06-23 23:33:18 +0530

    initial commit

---
+++ .babelrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src/'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'auto',
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+}
